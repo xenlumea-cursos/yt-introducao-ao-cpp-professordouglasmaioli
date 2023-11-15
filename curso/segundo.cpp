bb7dfc132ccd769d45635c03175e78477970f66c
Emanuel Costa Conrado <dev.conradoemanuelcosta@gmail.com>
2023-11-15 15:34:09 -0300

    Create segundo.cpp

---
+++ curso/segundo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int inteiro = 5;
+    cout << inteiro << endl;
+
+    float real;
+    real = 5.2;
+    cout << real << endl;
+
+    double real2;
+    real2 = 5.2e99;
+    cout << real2 << endl;
+
+    bool booleano;
+    booleano = true;
+    cout << booleano << endl;
+
+    char letra = 'bola';
+    cout << letra << endl;
+
+    string palavra = "bola";
+    cout << palavra << endl;
+
+    int idade;
+    cout << "Qual sua idade?" << endl;
+    cin >> idade;
+    cout << "Idade: " << idade << endl;
+
+    return 0;
+}
